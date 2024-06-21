8ba04b2cdc94e5d48c38e1dce67972dbf688c830
Christopher Garcia <chrisga@sas.upenn.edu>
2024-06-21 10:22:01 -0500

    revisions for lesson1_1 - removed uneeded imports - moved toDoList const outside of App function definition - added key property in place  of id property

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,28 @@
-import {useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+const todoList = [
+    {
+        id: 1,
+        title: 'Complete CTD Hawk assignment'
+    },
+    {
+        id: 2,
+        title: 'Call vet'
+    },
+    {
+        id: 3,
+        title: 'Cook family dinner'
+    }
+];
 
-    const todoList = [
-        {
-            id: 1,
-            title: 'Complete CTD Hawk assignment'
-        },
-        {
-            id: 2,
-            title: 'Call vet'
-        },
-        {
-            id: 3,
-            title: 'Cook family dinner'
-        }
-    ];
+function App() {
 
     return (
         <>
             <h1>Todo List</h1>
             <ul>
                 {
-                    todoList.map((el) => <li id={el.id}>{el.title}</li>)
+                    todoList.map((el) => <li key={el.id}>{el.title}</li>)
                 }
             </ul>
         </>
