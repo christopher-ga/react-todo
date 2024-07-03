de7be3ffcae5ef0e1da4a44aa6423351a96fad19
Christopher Garcia <chrisga@sas.upenn.edu>
2024-07-03 01:06:53 -0500

    created state for ToDo items

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css'
 import TodoList from "./TodoList.jsx";
 import AddTodoForm from "./AddTodoForm.jsx";
+import {useState} from "react";
 
 function App() {
 
+    const [newToDo, setNewToDo] = useState("");
 
     return (
         <>
             <h1>Todo List</h1>
-            <AddTodoForm></AddTodoForm>
+            <AddTodoForm onAddTodo={setNewToDo}></AddTodoForm>
+            <p>{newToDo}</p>
             <TodoList></TodoList>
         </>
     )
